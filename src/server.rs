72101e208a40a948d09e9f1b9b4aadb3deccc26b
subh <subhajitchaudhury05@gmail.com>
2025-12-11 17:12:34 +0530

    done with the imports

--- src/server.rs
+++ src/server.rs
@@ -1,1 +1,16 @@
+use tonic::{Request, Response, Status, transport::Server};
 
+// Include the generated protobuf code from proto/odin.proto
+pub mod odin {
+    tonic::include_proto!("odin");
+}
+
+// Import the generated types and server trait
+use odin::solana_tx_log_server::{SolanaTxLog, SolanaTxLogServer};
+use odin::{GetTxRequest, GetTxResponse, StreamProgramRequest, LogMessage, ComputeUnitLog, AnchorEvent};
+
+// The following types are now available from the generated code:
+// - GetTxRequest, GetTxResponse
+// - StreamProgramRequest
+// - LogMessage, ComputeUnitLog, AnchorEvent
+// - solana_tx_log_server::SolanaTxLog (the server trait)
